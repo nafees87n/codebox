5fc7f14484c360852c65a7a562836964b5ed87b8
aaryak-shah <aaryakdshah@gmail.com>
2020-12-28 20:54:12 +0530

    fixed execution error handling. added dockerignore

--- executor/lib/runCode.js
+++ executor/lib/runCode.js
@@ -3,47 +3,53 @@ const exec = util.promisify(require('child_process').exec)
 const fs = require('fs')
 
 const runCode = async (language, filePath) => {
-  console.log('runCode called');
+  console.log('runCode called')
   var result = { stderr: 'no output' }
   switch (language) {
     case 'cpp': {
-      console.log('c++ exec');
-      result = await exec(
-        `g++ -o ${filePath}.out ${filePath}.cpp && ${filePath}.out < ${filePath}`,
-        { timeout: 3000, maxBuffer: 1024 * 1024 * 5 }
-      )
-      fs.promises.unlink(filePath + '.out')
+      console.log('c++ exec')
+      try {
+        result = await exec(
+          `g++ -o ${filePath}.out ${filePath}.cpp && ${filePath}.out < ${filePath}`,
+          { timeout: 3000, maxBuffer: 1024 * 1024 * 5 }
+        )
+        fs.promises.unlink(filePath + '.out')
+      } catch (err) {
+        result = { stderr: err }
+      }
       break
     }
     case 'js': {
       console.log('js exec')
-      result = await exec(`node ${filePath}.js < ${filePath}`, {
-        timeout: 5000,
-        maxBuffer: 1024 * 1024 * 5,
-      })
+      try {
+        result = await exec(`node ${filePath}.js < ${filePath}`, {
+          timeout: 5000,
+          maxBuffer: 1024 * 1024 * 5,
+        })
+      } catch (err) {
+        result = { stderr: err }
+      }
       break
     }
     case 'py': {
-      console.log('runCode.js py switch');
-      console.log('executing');
-      result = await exec(`cat ${filePath} | python3 ${filePath}.py`, {
-        timeout: 5000,
-        maxBuffer: 1024 * 1024 * 5,
-      })
-      // result = await exec(`ls /storage/ -al`, {
-      //   timeout: 5000,
-      //   maxBuffer: 1024 * 1024 * 5,
-      // })
-      console.log('Result:');
-      console.log(result)
+      console.log('runCode.js py switch')
+      console.log('executing')
+      try {
+        result = await exec(`cat ${filePath} | python3 ${filePath}.py`, {
+          timeout: 5000,
+          maxBuffer: 1024 * 1024 * 5,
+        })
+      } catch (err) {
+        result = { stderr: err }
+      }
       break
     }
   }
 
   fs.promises.unlink(filePath)
   fs.promises.unlink(filePath + '.' + language)
-  console.log('returning from runCode() with result');
-  console.log(result);
+  // console.log('returning from runCode() with result');
+  // console.log(result);
   return result
 }
 
