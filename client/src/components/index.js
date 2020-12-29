d0b5ac94f14354f7df962bbd9376aefb72dd0899
Nafees Nehar <nafees87n@gmail.com>
2020-12-30 00:33:28 +0530

    editor setup done

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import Ace from './Ace'
-import CodeEditor from './CodeEditor'
+import React, { useState } from 'react'
+import Editor from './Editor'
+import AceEditor from 'react-ace'
+import 'ace-builds/src-noconflict/mode-python'
+import 'ace-builds/src-noconflict/mode-c_cpp'
+import 'ace-builds/src-noconflict/mode-javascript'
+import 'ace-builds/src-noconflict/theme-monokai'
+import 'ace-builds/src-noconflict/ext-language_tools'
 
+const languages = ['javascript', 'c++', 'python']
 const Homepage = () => {
+  const [mode, setMode] = useState('javascript')
+  const [code, setCode] = useState('')
+  const [input, setInput] = useState('')
   return (
     <div className="container-fluid m-0 p-0">
       <div className="row m-0 p-0 ">
@@ -10,11 +19,39 @@ const Homepage = () => {
           className="m-0 col-7 pr-1 p-0 d-flex flex-column"
           style={{ height: '100vh' }}
         >
-          <div style={{ background: 'black', color: 'white' }}>
-            <h1>Code</h1>
+          <div
+            className="d-flex justify-content-between"
+            style={{ background: 'black', color: 'white' }}
+          >
+            <h1 className="">Code</h1>
+            <select onChange={(e) => setMode(e.target.value)}>
+              {languages.map((lang) => (
+                <option key={lang} value={lang}>
+                  {lang}
+                </option>
+              ))}
+            </select>
+            <button>run</button>
           </div>
           <div style={{ width: '100%', height: '100%' }}>
-            <Ace />
+            <AceEditor
+              mode={mode}
+              theme="monokai"
+              height="100%"
+              width="100%"
+              value={code}
+              fontSize={18}
+              showPrintMargin={false}
+              onChange={(val) => setCode(val)}
+              // onChange={onChange}
+              name="UNIQUE_ID_OF_DIV"
+              //   editorProps={{ $blockScrolling: true }}
+              setOptions={{
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                enableSnippets: true,
+              }}
+            />
           </div>
         </div>
         <div className="col-5 p-0">
@@ -23,13 +60,33 @@ const Homepage = () => {
               <h1>Input</h1>
             </div>
             <div style={{ width: '100%', height: '100%' }}>
-              <Ace />
+              <AceEditor
+                mode="text"
+                theme="monokai"
+                height="100%"
+                width="100%"
+                value={input}
+                onChange={(val) => setInput(val)}
+                fontSize={18}
+                showPrintMargin={false}
+              />
             </div>
-            <div style={{ background: 'black', color: 'white' }}>
+            <div
+              className="pt-1 mt-1"
+              style={{ background: 'black', color: 'white' }}
+            >
               <h1>Output</h1>
             </div>
-            <div className="pt-1" style={{ width: '100%', height: '100%' }}>
-              <Ace />
+            <div style={{ width: '100%', height: '100%' }}>
+              <AceEditor
+                mode="text"
+                theme="monokai"
+                height="100%"
+                width="100%"
+                fontSize={18}
+                showPrintMargin={false}
+                readOnly
+              />
             </div>
           </div>
         </div>
