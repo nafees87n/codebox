8975df8c3df81339c63b08de81e8a13a93985249
Nafees Nehar <nafees87n@gmail.com>
2021-06-05 21:10:52 +0530

    pretty client

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -1,154 +1,154 @@
 // imports
 // react
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 // libraries
-import AceEditor from 'react-ace'
-import axios from 'axios'
-import openSocket from 'socket.io-client'
+import AceEditor from "react-ace";
+import axios from "axios";
+import openSocket from "socket.io-client";
 // ace
-import 'ace-builds/src-noconflict/mode-python'
-import 'ace-builds/src-noconflict/mode-c_cpp'
-import 'ace-builds/src-noconflict/mode-javascript'
-import 'ace-builds/src-noconflict/ext-language_tools'
-import 'ace-builds/src-noconflict/theme-cobalt'
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/theme-cobalt";
 // custom
-import useLocalStorage from '../../hooks/useLocalStorage'
-import ModalBox from '../ModalBox'
+import useLocalStorage from "../../hooks/useLocalStorage";
+import ModalBox from "../ModalBox";
 // style
-import '../../fonts/JetBrainsMono[wght].ttf'
-import './Homepage.css'
+import "../../fonts/JetBrainsMono[wght].ttf";
+import "./Homepage.css";
 
 // global vars
-var languages = ['python', 'c_cpp', 'javascript']
-const socket = openSocket('http://server:9000')
-const modes = { javascript: 'js', c_cpp: 'cpp', python: 'py' }
+var languages = ["python", "c_cpp", "javascript"];
+const socket = openSocket("http://localhost:9000");
+const modes = { javascript: "js", c_cpp: "cpp", python: "py" };
 const defaultCode = {
   javascript: "console.log('hello rce')",
   c_cpp:
     '#include <iostream>\n\nint main() {\n\tstd::cout << "hello rce";\n\treturn 0;\n}',
   python: "print('hello rce')",
-}
+};
 
 // component
 const Homepage = () => {
   // cached state vars
-  const [userCode, setUserCode] = useLocalStorage('userCode', '')
-  const [mode, setMode] = useLocalStorage('mode', 'python')
-  const [code, setCode] = useLocalStorage('code', defaultCode[mode])
-  const [input, setInput] = useLocalStorage('input', '')
+  const [userCode, setUserCode] = useLocalStorage("userCode", "");
+  const [mode, setMode] = useLocalStorage("mode", "python");
+  const [code, setCode] = useLocalStorage("code", defaultCode[mode]);
+  const [input, setInput] = useLocalStorage("input", "");
   const [joinedSessionCode, setJoinedSessionCode] = useLocalStorage(
-    'joinedSessionCode',
-    ''
-  )
+    "joinedSessionCode",
+    ""
+  );
   // non cached state vars
-  const [output, setOutput] = useState('')
+  const [output, setOutput] = useState("");
 
   //code run on press of F5
   useEffect(() => {
     document.onkeydown = (keyDownEvent) => {
-      if (keyDownEvent.altKey && keyDownEvent.key === 'F5')
-        document.getElementById('run-btn').click()
-    }
-  })
+      if (keyDownEvent.altKey && keyDownEvent.key === "F5")
+        document.getElementById("run-btn").click();
+    };
+  });
 
   // on load effect
   useEffect(() => {
-    var now = new Date()
-    var storeTime = JSON.parse(localStorage.getItem('na-rce-storeTime'))
+    var now = new Date();
+    var storeTime = JSON.parse(localStorage.getItem("na-rce-storeTime"));
     if (Math.ceil(Math.abs(now - storeTime) / (1000 * 60 * 60 * 24)) > 1) {
-      setUserCode('')
-      setJoinedSessionCode('')
+      setUserCode("");
+      setJoinedSessionCode("");
       axios
-        .get('/code')
+        .get("/code")
         .then(({ data }) => {
-          setUserCode(data)
+          setUserCode(data);
 
-          socket.emit('hostSession', {
+          socket.emit("hostSession", {
             channelID: data,
-          })
+          });
         })
-        .catch((e) => console.log('error', e))
+        .catch((e) => console.log("error", e));
       localStorage.setItem(
-        'na-rce-storeTime',
+        "na-rce-storeTime",
         JSON.stringify(new Date().getTime())
-      )
+      );
     }
-    if (userCode === '') {
+    if (userCode === "") {
       axios
-        .get('/code')
+        .get("/code")
         .then(({ data }) => {
-          setUserCode(data)
+          setUserCode(data);
 
-          socket.emit('hostSession', {
+          socket.emit("hostSession", {
             channelID: data,
-          })
+          });
         })
-        .catch((e) => console.log('error', e))
+        .catch((e) => console.log("error", e));
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   // on change in [joinedSessionCode] effect
   useEffect(() => {
-    socket.emit('joinSession', {
+    socket.emit("joinSession", {
       channelID: joinedSessionCode,
-    })
-  }, [joinedSessionCode])
+    });
+  }, [joinedSessionCode]);
 
   // on change in [code, output, mode, input, userCode, joinedSessionCode] effect
   useEffect(() => {
     if (!joinedSessionCode) {
-      socket.emit('realtime', {
+      socket.emit("realtime", {
         channelID: userCode,
         mode,
         code,
         input,
         output,
-      })
-      socket.on('initialLoad', () => {
-        socket.emit('realtime', {
+      });
+      socket.on("initialLoad", () => {
+        socket.emit("realtime", {
           channelID: userCode,
           mode,
           code,
           input,
           output,
-        })
-      })
+        });
+      });
     }
-  }, [code, output, mode, input, userCode, joinedSessionCode])
+  }, [code, output, mode, input, userCode, joinedSessionCode]);
 
   // on change in [joinedSessionCode] effect
   useEffect(() => {
     if (joinedSessionCode) {
-      socket.on('realReceive', (data) => {
-        setMode(data.mode)
-        setInput(data.input)
-        setOutput(data.output)
-        setCode(data.code)
-      })
+      socket.on("realReceive", (data) => {
+        setMode(data.mode);
+        setInput(data.input);
+        setOutput(data.output);
+        setCode(data.code);
+      });
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [joinedSessionCode])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [joinedSessionCode]);
 
   // language select handler
   const modeHandle = (e) => {
-    setCode(defaultCode[e.target.value])
-    setMode(e.target.value)
-  }
+    setCode(defaultCode[e.target.value]);
+    setMode(e.target.value);
+  };
 
   // code run button handler
   const handlerun = () => {
     axios
-      .post('/code', {
+      .post("/code", {
         key: userCode,
         language: modes[mode],
         input: input,
         code: code,
       })
       .then(({ data }) => {
-        setOutput(data.toString())
-      })
-  }
+        setOutput(data.toString());
+      });
+  };
 
   // component return
   return (
@@ -161,21 +161,21 @@ const Homepage = () => {
       </div>
       <div className="nav">
         <h1 id="brand">
-          {' '}
-          &gt;codeBox{' '}
-          {joinedSessionCode !== '' ? 'joined: ' + joinedSessionCode : ''}
+          {" "}
+          &gt;codeBox{" "}
+          {joinedSessionCode !== "" ? "joined: " + joinedSessionCode : ""}
         </h1>
         <div id="navigation">
-          {joinedSessionCode !== '' ? (
+          {joinedSessionCode !== "" ? (
             <button
               className="nav-btn"
               onClick={() => {
-                socket.close()
-                setJoinedSessionCode('')
-                setInput('')
-                setOutput('')
-                setCode(defaultCode[mode])
-                window.location.reload()
+                socket.close();
+                setJoinedSessionCode("");
+                setInput("");
+                setOutput("");
+                setCode(defaultCode[mode]);
+                window.location.reload();
               }}
             >
               <h2>disconnect</h2>
@@ -186,7 +186,7 @@ const Homepage = () => {
           <button
             className="nav-btn"
             onClick={() => {
-              document.getElementsByClassName('modal-bg')[0].id = ''
+              document.getElementsByClassName("modal-bg")[0].id = "";
             }}
           >
             <h2>session</h2>
@@ -195,7 +195,7 @@ const Homepage = () => {
             className="nav-btn"
             onClick={() =>
               window.open(
-                'https://github.com/nafees87n/codebox/blob/main/docs/DOCS.md'
+                "https://github.com/nafees87n/codebox/blob/main/docs/DOCS.md"
               )
             }
           >
@@ -203,7 +203,7 @@ const Homepage = () => {
           </button>
           <button
             className="nav-btn"
-            onClick={() => window.open('https://github.com/nafees87n/codebox')}
+            onClick={() => window.open("https://github.com/nafees87n/codebox")}
           >
             <h2>github</h2>
           </button>
@@ -213,7 +213,7 @@ const Homepage = () => {
         <div id="code-header">
           <h2 className="region-title">code</h2>
           <h2 className="region-title-divider">|</h2>
-          {joinedSessionCode === '' ? (
+          {joinedSessionCode === "" ? (
             <>
               <select
                 id="language-select"
@@ -226,13 +226,13 @@ const Homepage = () => {
                     <option key={lang} value={lang}>
                       {lang.toUpperCase()}
                     </option>
-                  )
+                  );
                 })}
               </select>
               <h2 className="region-title-divider">|</h2>
               <button id="run-btn" onClick={handlerun}>
                 RUN (ALT + F5)
-              </button>{' '}
+              </button>{" "}
             </>
           ) : (
             <>
@@ -256,8 +256,8 @@ const Homepage = () => {
               enableLiveAutocompletion: true,
               enableSnippets: true,
             }}
-            highlightActiveLine={joinedSessionCode === '' ? true : false}
-            readOnly={joinedSessionCode === '' ? false : true}
+            highlightActiveLine={joinedSessionCode === "" ? true : false}
+            readOnly={joinedSessionCode === "" ? false : true}
           />
         </div>
       </div>
@@ -277,8 +277,8 @@ const Homepage = () => {
             fontSize={18}
             showPrintMargin={false}
             showGutter={false}
-            highlightActiveLine={joinedSessionCode === '' ? true : false}
-            readOnly={joinedSessionCode === '' ? false : true}
+            highlightActiveLine={joinedSessionCode === "" ? true : false}
+            readOnly={joinedSessionCode === "" ? false : true}
           />
         </div>
       </div>
@@ -303,7 +303,7 @@ const Homepage = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
