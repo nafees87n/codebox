b70b0e954e2ecf51580e71ddaf5d1bfc9fc95f1f
kailash360 <kailashkejriwal21@gmail.com>
2021-10-04 13:24:10 +0530

    fix: fixed indentation to 2 tabs

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,16 +1,16 @@
 module.exports = {
-    PREFIX: 'na-rce-',
-    SOCKET: "http://localhost:9000",
-    DOCS: "https://github.com/nafees87n/codebox/blob/main/docs/DOCS.md",
-    GITHUB_URL: "https://github.com/nafees87n/codebox",
-    MODES: {
-        javascript: "js",
-        c_cpp: "cpp",
-        python: "py"
-    },
-    DEFAULT_CODE: {
-        javascript: "console.log('hello rce')",
-        c_cpp: '#include <iostream>\n\nint main() {\n\tstd::cout << "hello rce";\n\treturn 0;\n}',
-        python: "print('hello rce')",
-    }
+  PREFIX: 'na-rce-',
+  SOCKET: "http://localhost:9000",
+  DOCS: "https://github.com/nafees87n/codebox/blob/main/docs/DOCS.md",
+  GITHUB_URL: "https://github.com/nafees87n/codebox",
+  MODES: {
+    javascript: "js",
+    c_cpp: "cpp",
+    python: "py"
+  },
+  DEFAULT_CODE: {
+    javascript: "console.log('hello rce')",
+    c_cpp: '#include <iostream>\n\nint main() {\n\tstd::cout << "hello rce";\n\treturn 0;\n}',
+    python: "print('hello rce')",
+  }
 }
