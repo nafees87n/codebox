57157fb84b1de8cd750042b534360ba4c7825fef
aaryak-shah <aaryak-shah@users.noreply.github.com>
2021-10-06 13:25:28 +0000

    Prettified Code!

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -150,17 +150,17 @@ const Homepage = () => {
   }
 
   /* states and fucntions to control the responsiveness of the screen */
-  const [visible,setVisible] = useState(true)
-  
+  const [visible, setVisible] = useState(true)
+
   //Check whether teh current screen size is desktop or mobile
   useEffect(() => {
-    if(window.screen.width<500){
+    if (window.screen.width < 500) {
       setVisible(false)
     }
-  },[])
-  
+  }, [])
+
   //function to toggle menu for mobile view
-  const toggleMenu=()=>{
+  const toggleMenu = () => {
     setVisible(!visible)
   }
 
@@ -180,15 +180,12 @@ const Homepage = () => {
           {joinedSessionCode !== '' ? 'joined: ' + joinedSessionCode : ''}
         </h1>
         <div id="navigation" className="hide_menu">
-        <button
-            className="nav-btn menu_btn"
-            onClick={toggleMenu}
-          >
+          <button className="nav-btn menu_btn" onClick={toggleMenu}>
             <h2>Menu</h2>
           </button>
           {joinedSessionCode !== '' ? (
             <button
-              style={{display:visible?'block':'none'}}
+              style={{ display: visible ? 'block' : 'none' }}
               className="nav-btn"
               onClick={() => {
                 socket.close()
@@ -205,7 +202,7 @@ const Homepage = () => {
             <></>
           )}
           <button
-            style={{display:visible?'block':'none'}}
+            style={{ display: visible ? 'block' : 'none' }}
             className="nav-btn"
             onClick={() => {
               document.getElementsByClassName('modal-bg')[0].id = ''
@@ -214,21 +211,21 @@ const Homepage = () => {
             <h2>session</h2>
           </button>
           <button
-            style={{display:visible?'block':'none'}}
+            style={{ display: visible ? 'block' : 'none' }}
             className="nav-btn"
             onClick={() => window.open(CONSTANTS.DOCS)}
           >
             <h2>docs</h2>
           </button>
           <button
-            style={{display:visible?'block':'none'}}
+            style={{ display: visible ? 'block' : 'none' }}
             className="nav-btn"
             onClick={() => window.open(CONSTANTS.GITHUB_URL)}
           >
             <h2>github</h2>
           </button>
           <button
-            style={{display:visible?'block':'none'}}
+            style={{ display: visible ? 'block' : 'none' }}
             className="nav-btn"
             onClick={() => {
               localStorage.clear()
